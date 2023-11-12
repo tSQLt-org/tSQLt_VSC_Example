f8f44d64ed9d953569a2ccfc5432d20d52b0a298
“mbt1” <mbt1@users.noreply.github.com>
2023-11-12 20:16:52 +0000

    3rd test passing

--- Demo/dbo.GetDiscount.sql
+++ Demo/dbo.GetDiscount.sql
@@ -6,4 +6,4 @@ CREATE FUNCTION dbo.GetDiscount(@amount DECIMAL(13,2))
 RETURNS TABLE
 AS
 RETURN
-  SELECT CASE WHEN @amount>50 THEN @amount*.1 ELSE 0 END Discount;
+  SELECT CASE WHEN @amount>=50 THEN @amount*.1 ELSE 0 END Discount;
